efe7bb7dbd920602c6ae8a6ccfaf13bdd5586670
Denis <denishome21@gmail.com>
2024-02-21 23:45:38 +0500

    Добавьте файлы проекта.

---
+++ OmniLine/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OmniLine.Models
+{
+    public class Contact
+    {
+        [Key]
+        public long Id { get; set; }
+        public string DateCreate { get; set; }
+        public string DateEdit { get; set; }
+        public string FIO { get; set; }
+        public string Email { get; set; }
+        [ForeignKey("CounterAgent")]
+        public long? CounterAgentId { get; set; }
+        public int? CounterAgent { get; set; }
+    }
+}
---
+++ OmniLine/Models/CounterAgent.cs
@@ -0,0 +1,12 @@
+﻿namespace OmniLine.Models
+{
+    public class CounterAgent
+    {
+        public long CounterAgentId { get; set; }
+        public string Name { get; set; }
+        public string DateCreate { get; set; }
+        public string DateEdit { get; set; }
+        public ICollection<Contact> Contacts { get; set; }
+
+    }
+}
