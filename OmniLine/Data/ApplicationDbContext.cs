0359d1ef7daef6b71141da89f589ed3d749ef188
Denis <denishome21@gmail.com>
2024-02-21 23:56:01 +0500

    add Db Context

---
+++ OmniLine/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using OmniLine.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace OmniLine.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<CounterAgent> CounterAgents { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Contact>().HasIndex(e =>e.Email).IsUnique();
+            modelBuilder.Entity<CounterAgent>().HasIndex(c=>c.Name).IsUnique();
+        }
+
+
+    }
+}
